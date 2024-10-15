f607cafbdbfca15ee5990b692368508001020825
Victor Fernandes <victor.m.fernandes@outlook.com>
2024-10-15 08:54:00 -0300

    ♻️ refactor program into using startup class

--- src/Mfm.Api/Program.cs
+++ src/Mfm.Api/Program.cs
@@ -1,35 +1,12 @@
-using Mfm.Api.Configuration.ResponseStandardization;
-using Mfm.Application.Configuration;
-using Mfm.Infrastructure.Data.Configuration;
-using Mfm.Infrastructure.Messaging.Configuration;
+using Mfm.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-_ = builder.Services.AddControllers();
-
-_ = builder.Services.AddEndpointsApiExplorer();
-_ = builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(TimeProvider.System);
-
-builder.Services.ConfigureApplication();
-builder.Services.ConfigureData(builder.Configuration);
-builder.Services.ConfigureMessaging(builder.Configuration);
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    _ = app.UseSwagger();
-    _ = app.UseSwaggerUI();
-    app.ApplyMigrations();
-}
-
-_ = app.UseMiddleware<ErrorMiddleware>();
-
-_ = app.UseHttpsRedirection();
-
-_ = app.UseAuthorization();
-
-_ = app.MapControllers();
+startup.Configure(app);
 
 app.Run();
---
+++ src/Mfm.Api/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Mfm.Api.Configuration.ResponseStandardization;
+using Mfm.Application.Configuration;
+using Mfm.Infrastructure.Data.Configuration;
+using Mfm.Infrastructure.Messaging.Configuration;
+
+namespace Mfm.Api;
+
+public sealed class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        _ = services.AddControllers();
+
+        _ = services.AddEndpointsApiExplorer();
+        _ = services.AddSwaggerGen();
+        services.AddSingleton(TimeProvider.System);
+
+        services.ConfigureApplication();
+        services.ConfigureData(_configuration);
+        services.ConfigureMessaging(_configuration);
+    }
+
+    public void Configure(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            _ = app.UseSwagger();
+            _ = app.UseSwaggerUI();
+            app.ApplyMigrations();
+        }
+
+        _ = app.UseMiddleware<ErrorMiddleware>();
+
+        _ = app.UseHttpsRedirection();
+
+        _ = app.UseAuthorization();
+
+        _ = app.MapControllers();
+    }
+}
