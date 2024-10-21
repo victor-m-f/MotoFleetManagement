912619d2d48c8f31cf02225c528360e76066278e
Victor Fernandes <victor.m.fernandes@outlook.com>
2024-10-21 09:36:34 -0300

    ♻️ bring ConvertBase64ToBytes to a helper class to be reusable

---
+++ src/Mfm.Application/Helpers/ImageHelper.cs
@@ -0,0 +1,36 @@
+﻿using Mfm.Domain.Exceptions;
+
+namespace Mfm.Application.Helpers;
+internal static class ImageHelper
+{
+    public static (string fileName, byte[] fileBytes) ConvertBase64ToBytes(string base64String, string id)
+    {
+        if (string.IsNullOrWhiteSpace(base64String))
+        {
+            throw new ValidationException("The image string cannot be empty.");
+        }
+
+        var prefixMap = new Dictionary<string, string>
+        {
+            { "data:image/png;base64,", "png" },
+            { "data:image/bmp;base64,", "bmp" }
+        };
+
+        var prefix = prefixMap.Keys
+            .FirstOrDefault(p => base64String.StartsWith(p, StringComparison.OrdinalIgnoreCase));
+
+        var cleanBase64 = prefix == null ? base64String : base64String[prefix.Length..];
+
+        try
+        {
+            var fileBytes = Convert.FromBase64String(cleanBase64);
+            var extension = prefix == null ? "png" : prefixMap[prefix];
+            var fileName = $"{id}.{extension}";
+            return (fileName, fileBytes);
+        }
+        catch (FormatException ex)
+        {
+            throw new ValidationException("The provided image is not a valid base64 string.", ex);
+        }
+    }
+}
--- src/Mfm.Application/UseCases/DeliveryPersons/CreateDeliveryPerson/CreateDeliveryPersonUseCase.cs
+++ src/Mfm.Application/UseCases/DeliveryPersons/CreateDeliveryPerson/CreateDeliveryPersonUseCase.cs
@@ -1,4 +1,5 @@
-﻿using Mfm.Application.UseCases.Base;
+﻿using Mfm.Application.Helpers;
+using Mfm.Application.UseCases.Base;
 using Mfm.Domain.Entities;
 using Mfm.Domain.Entities.ValueObjects;
 using Mfm.Domain.Exceptions;
@@ -51,7 +52,7 @@ internal sealed class CreateDeliveryPersonUseCase : UseCaseBase, ICreateDelivery
             return CreateDeliveryPersonOutput.CreateSameCnhNumberError();
         }
 
-        var (fileName, fileBytes) = ConvertBase64ToBytes(
+        var (fileName, fileBytes) = ImageHelper.ConvertBase64ToBytes(
             request.DeliveryPerson.CnhImage,
             request.DeliveryPerson.Id);
 
@@ -81,36 +82,5 @@ internal sealed class CreateDeliveryPersonUseCase : UseCaseBase, ICreateDelivery
         }
 
         return new CreateDeliveryPersonOutput();
-    }
-
-    private static (string fileName, byte[] fileBytes) ConvertBase64ToBytes(string base64String, string id)
-    {
-        if (string.IsNullOrWhiteSpace(base64String))
-        {
-            throw new ValidationException("The image string cannot be empty.");
-        }
-
-        var prefixMap = new Dictionary<string, string>
-        {
-            { "data:image/png;base64,", "png" },
-            { "data:image/bmp;base64,", "bmp" }
-        };
-
-        var prefix = prefixMap.Keys
-            .FirstOrDefault(p => base64String.StartsWith(p, StringComparison.OrdinalIgnoreCase));
-
-        var cleanBase64 = prefix == null ? base64String : base64String[prefix.Length..];
-
-        try
-        {
-            var fileBytes = Convert.FromBase64String(cleanBase64);
-            var extension = prefix == null ? "png" : prefixMap[prefix];
-            var fileName = $"{id}.{extension}";
-            return (fileName, fileBytes);
-        }
-        catch (FormatException ex)
-        {
-            throw new ValidationException("The provided image is not a valid base64 string.", ex);
-        }
     }
 }
